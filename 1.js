a96468f15549c54592e8fbffb7978c424fe147b0
enghelalbd <informhelal@yahoo.com>
2025-08-14 06:29:12 +0600

    first commit

---
+++ 1.js
@@ -0,0 +1,31 @@
+function totalFine( fare ) {    
+    
+ if(fare>0){
+   
+    
+const  penanty=fare+fare*0.2+30
+         
+         return penanty
+ }
+ else if (typeof fare === 'string'){
+    
+  return "Invalid" 
+ }
+         
+else if(fare<0){
+    return "Invalid"
+}
+       
+}
+
+totalFine('Hello')
+totalFine(-100)
+
+
+
+console.log(totalFine('Hello'));
+console.log(totalFine(100));
+console.log(totalFine(-100));
+
+
+
---
+++ 2.js
@@ -0,0 +1,12 @@
+function  onlyCharacter( str ) {
+    let re=str.trim();
+
+   console.log(re)
+
+
+
+}
+
+onlyCharacter('hjkdsjhgh jhjkhdfj jkljdj')
+
+
